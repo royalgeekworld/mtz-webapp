caa763ce42630945e21a97dffb5c79c58bfc497c
Matt A. Tobin <email@mattatobin.com>
2020-03-23 23:14:26 -0400

    Add basic code

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// == | Setup | =======================================================================================================
+
+error_reporting(E_ALL);
+ini_set("display_errors", "on");
+
+require_once('./globalConstants.php');
+require_once('./globalFunctions.php');
+
+// ====================================================================================================================
+
+// == | Main | ========================================================================================================
+
+// Define an array that will hold the current application state
+$gaRuntime = array(
+  'authentication'      => null,
+  'currentApplication'  => null,
+  'orginalApplication'  => null,
+  'currentSiteTitle'    => null,
+  'currentScheme'       => gfSuperVar('server', 'SCHEME'),
+  'currentDomain'       => null,
+  'debugMode'           => null,
+  'tap'                 => gfSuperVar('var', file_exists(ROOT_PATH . '/.tap')),
+  'phpServerName'       => gfSuperVar('server', 'SERVER_NAME'),
+  'phpRequestURI'       => gfSuperVar('server', 'REQUEST_URI'),
+  'remoteAddr'          => gfSuperVar('server', 'HTTP_X_FORWARDED_FOR') ??
+                           gfSuperVar('server', 'REMOTE_ADDR'),
+  'requestComponent'    => gfSuperVar('get', 'component'),
+  'requestPath'         => gfSuperVar('get', 'path'),
+  'requestDebugOff'     => gfSuperVar('get', 'debugOff'),
+  'requestSearchTerms'  => gfSuperVar('get', 'terms')
+);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// Root (/) won't set a component or path
+if (!$gaRuntime['requestComponent'] && !$gaRuntime['requestPath']) {
+  $gaRuntime['requestComponent'] = 'site';
+  $gaRuntime['requestPath'] = '/';
+}
+// The SPECIAL component overrides the SITE component
+elseif (str_starts_with($gaRuntime['phpRequestURI'], '/special/')) {
+  $gaRuntime['requestComponent'] = 'special';
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// Load component based on requestComponent
+if ($gaRuntime['requestComponent'] && array_key_exists($gaRuntime['requestComponent'], COMPONENTS)) {
+  require_once(COMPONENTS[$gaRuntime['requestComponent']]);
+}
+else {
+  if (!$gaRuntime['debugMode']) {
+    gfHeader(404);
+  }
+  gfError('Invalid component');
+}
+
+// ====================================================================================================================
+
+?>
