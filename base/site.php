933257da81b74d59ed462f31a37e9f92397c2ad0
Matt A. Tobin <email@mattatobin.com>
2020-11-10 23:13:08 -0500

    Add and update many things.

--- base/site.php
+++ base/site.php
@@ -24,27 +24,30 @@ const CONTENT = array(
   '/build/sunos/'                             => ['build-sunos', 'Building Pale Moon: SunOS-based Systems'],
 
   // Add-ons
-  '/addons/'                                  => ['addons-index', 'Add-ons', true],
-  '/addons/resources/'                        => ['addons-resources', 'Add-ons: Resources', true],
-  '/addons/resources/install-manifest/'       => ['addons-resources-install-manifest', 'Add-ons: Install Manifest', true],
+  '/addons/'                                  => ['addons-index', 'Add-ons: Overview'],
+  '/addons/concepts/'                         => ['addons-concepts', 'Add-ons: Concepts', true],
   '/addons/extensions/'                       => ['addons-extensions', 'Add-ons: Extensions', true],
-  '/addons/extensions/jetpack/'               => ['addons-extensions-jetpack', 'Add-ons: Jetpack Style Extensions', true],
   '/addons/themes/'                           => ['addons-themes', 'Add-ons: Themes'],
   '/addons/themes/complete/'                  => ['addons-themes-complete', 'Add-ons: Complete Themes'],
   '/addons/themes/personas/'                  => ['addons-themes-personas', 'Add-ons: Personas (Lightweight Themes)'],
   '/addons/themes/pm-history/'                => ['addons-themes-pmhistory', 'Add-ons: Pale Moon Theme History'],
   '/addons/site/'                             => ['addons-site', 'Add-ons: Site'],
-  '/addons/site/phoebus-code/'                => ['addons-site-phoebus-code', 'Add-ons: Phoebus Code Syntax'],
 
   // Misc docs that need a place to live
   '/docs/'                                    => ['docs-index', 'Docs', true],
   '/docs/bounty/'                             => ['docs-bounty', 'Development Bounty Program'],
+  '/docs/syntax/'                             => ['docs-syntax', 'Site-Specific Syntax'],
 );
 
-// This constant array contains old wiki URLs (accounting for the fact that we add / to non-file.ext urls here)
-const WIKI_URLS = array(
-  '/Main_Page/' => '/',
-  '/Developer_Guide/' => '/',
+// This constant array contains redirects (largely old wiki urls accounting for the fact that we add / to non-file.ext urls here)
+const REDIRECTS = array(
+  // Obsolete URLs since the Developer Site Started
+  '/addons/resources/'                                      => '/addons/concepts/',
+  '/addons/site/phoebus-code/'                              => '/docs/syntax/#phoebusCode',
+
+  // Old Wiki URLS
+  '/Main_Page/'                                             => '/',
+  '/Developer_Guide/'                                       => '/',
   '/Developer_Guide:Build_Instructions/'                    => '/build/',
   '/Developer_Guide:Build_Instructions/Pale_Moon/'          => '/build/',
   '/Developer_Guide:Build_Instructions/Pale_Moon/Windows/'  => '/build/windows/',
@@ -54,15 +57,14 @@ const WIKI_URLS = array(
   '/Add-ons:Extensions/'                                    => '/addons/extensions/',
   '/Add-ons:Extensions/Toolkit/'                            => '/addons/extensions/',
   '/Add-ons:Extensions/Bootstrap/'                          => '/addons/extensions/',
-  '/Add-ons:Extensions/Jetpack/'                            => '/addons/extensions/jetpack/',
+  '/Add-ons:Extensions/Jetpack/'                            => '/addons/extensions/',
   '/Add-ons:Themes/'                                        => '/addons/themes/',
   '/Add-ons:Themes/Complete/'                               => '/addons/themes/complete/',
   '/Add-ons:Themes/Complete/Changes/'                       => '/addons/themes/pm-history/',
   '/Add-ons:Themes/Persona/'                                => '/addons/themes/personas/',
   '/Add-ons:Site/'                                          => '/addons/site/',
   '/Add-ons:Site/Submit/'                                   => '/addons/site/',
-  '/Add-ons:Site/Manifest_Files/'                           => '/addons/site/phoebus-code/',
-  
+  '/Add-ons:Site/Manifest_Files/'                           => '/docs/syntax/#phoebusCode',
 );
 
 require_once(MODULES['content']);
@@ -76,9 +78,9 @@ if (array_key_exists($gaRuntime['requestPath'], CONTENT)) {
   generateContent($gaRuntime['requestPath']);
 }
 
-// This will handle translation from the wiki urls to the new urls
-if (array_key_exists($gaRuntime['requestPath'], WIKI_URLS)) {
-  gfRedirect(WIKI_URLS[$gaRuntime['requestPath']]);
+// This will handle redirects largely from the old wiki urls to the new urls
+if (array_key_exists($gaRuntime['requestPath'], REDIRECTS)) {
+  gfRedirect(REDIRECTS[$gaRuntime['requestPath']]);
 }
 
 // No idea what we should do so 404
