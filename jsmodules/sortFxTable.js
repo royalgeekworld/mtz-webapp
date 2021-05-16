5682575ce581def5dafcbe90d98be0987e9c18c5
Matt A. Tobin <email@mattatobin.com>
2021-05-16 11:23:42 -0400

    FINALLY FIX THE THINGS

--- jsmodules/sortFxTable.js
+++ jsmodules/sortFxTable.js
@@ -1,5 +1,5 @@
 function sortFxTable() {
-  var table, i, x, y;
+  var table, i, j, x, y;
   table = document.getElementById("fxtable");
   var switching = true;
    
@@ -7,17 +7,17 @@ function sortFxTable() {
   var NoDR = rows.length - 1;
    
   // Run loop through all rows (place element)
-  for (i = 2; i <= NoDR; i++) {
+  for (i = 1; i <= NoDR; i++) {
     switching = false;
     x = rows[i].getElementsByTagName("TD")[1].getElementsByTagName("A")[0];
    
     // Run loop through all rows (check element)
-    for (j = 1; j < NoDR; j++) {
+    for (j = 1; j <= NoDR; j++) {
       // Fetch element that needs to be compared
       y = rows[j].getElementsByTagName("TD")[1].getElementsByTagName("A")[0];
     
       // Check if row needs to be inserted
-      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+      if (x.innerHTML.toLowerCase() <= y.innerHTML.toLowerCase()) {
         // If yes, break
         switching = true;
         break;
@@ -25,8 +25,9 @@ function sortFxTable() {
     }
     if (switching) {
       rows[i].parentNode.insertBefore(rows[i], rows[j]);
+    } else {
+      // element was already in its proper place.
     }
-    else {}
   }
 }
 
