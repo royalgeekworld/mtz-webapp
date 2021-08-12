3fd7cb4c1ffcce81395c9f64c571a76a0cbae872
Matt A. Tobin <email@mattatobin.com>
2021-08-12 01:13:40 -0400

    Put this incarnation of fundamentals under MPL

--- fundamentals.php
+++ fundamentals.php
@@ -1,4 +1,8 @@
 <?php
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 // == | Setup | =======================================================================================================
 
 // Check if the basic defines have been defined in the including script
